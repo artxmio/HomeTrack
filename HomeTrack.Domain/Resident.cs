d6499a9bcad6193909dd4ecf87a3b8c356ea87c0
artxmix <artem.artusevskij01@gmail.com>
2025-06-14 13:27:28 +0300

    Structure

---
+++ HomeTrack.Domain/Resident.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeTrack.Domain;
+
+public class Resident
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public DateTime CreateDate { get; set; }
+    public DateTime? UpdateDate { get; set; }
+}
