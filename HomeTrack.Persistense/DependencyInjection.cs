9cceb379497382b35baf0aeabdcea5b4b4c92c13
artxmix <artem.artusevskij01@gmail.com>
2025-06-17 12:55:19 +0300

    Program.cs

--- HomeTrack.Persistense/DependencyInjection.cs
+++ HomeTrack.Persistense/DependencyInjection.cs
@@ -2,7 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System.Runtime.CompilerServices;
 
 namespace HomeTrack.Persistense;
 
--- HomeTrack.WebApi/Program.cs
+++ HomeTrack.WebApi/Program.cs
@@ -1,10 +1,31 @@
+using HomeTrack.Application;
+using HomeTrack.Application.Common.Mappings;
+using HomeTrack.Application.Interfaces;
+using HomeTrack.Persistense;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddAutoMapper(config =>
+{
+    config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+    config.AddProfile(new AssemblyMappingProfile(typeof(IHomeTrackDbContext).Assembly));
+});
+
+builder.Services.AddApplication();
+
+builder.Services.AddPersistence(builder.Configuration);
+
+using var scope = builder.Services.BuildServiceProvider().CreateScope();
+
+var serviceProvider = scope.ServiceProvider;
+
+var context = serviceProvider.GetRequiredService<HomeTrackDbContext>();
+
+DbInitializer.Initialize(context);
 
 var app = builder.Build();
 
