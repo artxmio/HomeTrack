f6e11b521e482c8b0af32de89b609e27e82da1ff
artxmix <artem.artusevskij01@gmail.com>
2025-06-17 12:19:08 +0300

    DI

---
+++ HomeTrack.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using HomeTrack.Application.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace HomeTrack.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(provider => provider.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        
+        return services;
+    }
+}
---
+++ HomeTrack.Persistense/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using HomeTrack.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.CompilerServices;
+
+namespace HomeTrack.Persistense;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString = configuration["DbConnection"];
+
+        services.AddDbContext<HomeTrackDbContext>(options =>
+        {
+            options.UseMySql(connectionString);
+        });
+
+        services.AddScoped<IHomeTrackDbContext>(profider => 
+                                                profider.GetService<HomeTrackDbContext>() 
+                                             ?? throw new NullReferenceException("profider cant't be null"));
+
+        return services;
+    }
+}
