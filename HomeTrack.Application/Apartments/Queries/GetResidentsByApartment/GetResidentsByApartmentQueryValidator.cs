7e00346c171b48026083919fe5fb817fb203ba58
artxmix <artem.artusevskij01@gmail.com>
2025-07-22 16:19:13 +0300

    Apartment: validator for GetResidentListForApartmentQuery

---
+++ HomeTrack.Application/Apartments/Queries/GetResidentsByApartment/GetResidentsByApartmentQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace HomeTrack.Application.Apartments.Queries.GetResidentsByApartment;
+
+public class GetResidentsByApartmentQueryValidator
+    : AbstractValidator<GetResidentsByApartmentQuery>
+{
+    public GetResidentsByApartmentQueryValidator()
+    {
+        RuleFor(query => query.ApartmentId)
+            .NotEqual(Guid.Empty)
+            .WithMessage("ApartmentId must not be an empty GUID");
+    }
+}
